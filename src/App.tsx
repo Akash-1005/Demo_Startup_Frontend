a47f0221644abc19817c371490c0055084b37312
Akash Singh <akashsingh1052002@gmail.com>
2025-04-09 13:12:04 -0700

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import StethoscopeLanding from './Healthcarestartuplanding';
+import StethoscopeLanding from './HealthcareStartupLanding';
+
 
 function App() {
   return (
